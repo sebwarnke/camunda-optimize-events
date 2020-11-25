b0c65b89bba3f309af0cf2fa6439d70b2b3474ef
Sebastian Warnke <sebastian.warnke@camunda.com>
2020-11-25 11:42:35 +0100

    commit poc state

--- src/main/java/com/camunda/consulting/examples/Application.java
+++ src/main/java/com/camunda/consulting/examples/Application.java
@@ -1,13 +1,31 @@
 package com.camunda.consulting.examples;
 
+import com.camunda.consulting.simulator.SimulationExecutor;
+import com.camunda.consulting.simulator.SimulatorPlugin;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.joda.time.DateTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+        SimulationExecutor.execute(DateTime.now().minusMonths(1).toDate(), DateTime.now().toDate());
+    }
 
+    @Bean
+    public SimulatorPlugin simulatorPlugin() {
+        return new SimulatorPlugin();
+    }
+
+    @Bean
+    JavaDelegate doWork() {
+        return delegateExecution -> {
+            System.out.println("Step done");
+        };
+    }
 }
